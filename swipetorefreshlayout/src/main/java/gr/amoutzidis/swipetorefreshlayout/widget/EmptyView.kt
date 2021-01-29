aeaf208d7e37c84f05454c917b05da670a200030
amoutzidis <amoutzidis.d@gmail.com>
2021-01-29 20:13:33 +0200

    Initial commit

---
+++ swipetorefreshlayout/src/main/java/gr/amoutzidis/swipetorefreshlayout/widget/EmptyView.kt
@@ -0,0 +1,18 @@
+package gr.amoutzidis.swipetorefreshlayout.widget
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+
+class EmptyView: View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        setBackgroundColor(Color.BLUE)
+    }
+}
